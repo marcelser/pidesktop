47d8d2b49fa706dd5d22b42d5b255d8c8370ba08
Marc Elser <melser_regs@gmxpro.net>
2025-09-22 02:05:59 +0200

    Update pd-fixrtc.py copilot version

--- pidesktop-base/usr/share/pidesktop/python/pd-fixrtc.py
+++ pidesktop-base/usr/share/pidesktop/python/pd-fixrtc.py
@@ -1,153 +1,132 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
-#
-# brute force
-#
-
-import os;
+import os
 
 def runCommand(command):
-    print(command);
-    r = os.popen(command);
-    info = r.readlines();
-    for line in info:
-        print(line);
+    print(command)
+    with os.popen(command) as r:
+        for line in r:
+            print(line.strip())
 
 def updategpio6():
-    filename = '/boot/firmware/config.txt';
-    key = 'gpio';
+    filename = '/boot/firmware/config.txt'
+    key = 'gpio'
     value = '6=op,pn,dl'
-    fr = open(filename,'r');
-    try:
-        lines = fr.readlines();
-        update = 0;
-        newValue = '';
-        for line in lines :
-            if line and line.count(key) > 0:
-                if not line.strip().startswith('#'):
-                    sps = line.split('=',2);
-                    if len(sps) == 2 and sps[1] == value:
-                        update = 1;
-                    elif update == 0:
-                        newValue += key +'='+value+'\r\n';
-                        update = 2;
-            else:
-                newValue += line;
-        if not update == 1:
-            if update == 0:
-                newValue += '\r\n' + key+'='+ value;
-            fw = open(filename,'wb');
-            try:
-                fw.write(newValue);
-            finally:
-                fw.close();
-    finally:
-        fr.close();
+    update = 0
+    newValue = ''
+
+    with open(filename, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+
+    for line in lines:
+        if key in line:
+            if not line.strip().startswith('#'):
+                sps = line.strip().split('=', 1)
+                if len(sps) == 2 and sps[1] == value:
+                    update = 1
+                elif update == 0:
+                    newValue += f'{key}={value}\n'
+                    update = 2
+        else:
+            newValue += line
+
+    if update != 1:
+        if update == 0:
+            newValue += f'\n{key}={value}\n'
+        with open(filename, 'w', encoding='utf-8') as fw:
+            fw.write(newValue)
 
 def updategpio13():
-    filename = '/boot/firmware/config.txt';
-    key = 'gpio';
+    filename = '/boot/firmware/config.txt'
+    key = 'gpio'
     value = '13=ip'
-    fr = open(filename,'r');
-    try:
-        lines = fr.readlines();
-        update = 0;
-        newValue = '';
-        for line in lines :
-            if line and line.count(key) > 0:
-                if not line.strip().startswith('#'):
-                    sps = line.split('=',2);
-                    if len(sps) == 2 and sps[1] == value:
-                        update = 1;
-                    elif update == 0:
-                        newValue += key +'='+value+'\r\n';
-                        update = 2;
-            else:
-                newValue += line;
-        if not update == 1:
-            if update == 0:
-                newValue += '\r\n' + key+'='+ value;
-            fw = open(filename,'wb');
-            try:
-                fw.write(newValue);
-            finally:
-                fw.close();
-    finally:
-        fr.close();
+    update = 0
+    newValue = ''
 
+    with open(filename, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+
+    for line in lines:
+        if key in line:
+            if not line.strip().startswith('#'):
+                sps = line.strip().split('=', 1)
+                if len(sps) == 2 and sps[1] == value:
+                    update = 1
+                elif update == 0:
+                    newValue += f'{key}={value}\n'
+                    update = 2
+        else:
+            newValue += line
+
+    if update != 1:
+        if update == 0:
+            newValue += f'\n{key}={value}\n'
+        with open(filename, 'w', encoding='utf-8') as fw:
+            fw.write(newValue)
 
 def updateConfig():
-    filename = '/boot/firmware/config.txt';
-    key = 'dtoverlay';
+    filename = '/boot/firmware/config.txt'
+    key = 'dtoverlay'
     value = 'i2c-rtc,pcf8563'
-    fr = open(filename,'r');
-    try:
-        lines = fr.readlines();
-        update = 0;
-        newValue = '';
-        for line in lines :
-            if line and line.count(key) > 0:
-                if not line.strip().startswith('#'):
-                    sps = line.split('=',2);
-                    if len(sps) == 2 and sps[1] == value:
-                        update = 1;
-                    elif update == 0:
-                        newValue += key +'='+value+'\r\n';
-                        update = 2;
-            else:
-                newValue += line;
-        if not update == 1:
-            if update == 0:
-                newValue += '\r\n' + key+'='+ value;
-            fw = open(filename,'w');
-            try:
-                fw.write(newValue);
-            finally:
-                fw.close();
-    finally:
-        fr.close();
+    update = 0
+    newValue = ''
+
+    with open(filename, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+
+    for line in lines:
+        if key in line:
+            if not line.strip().startswith('#'):
+                sps = line.strip().split('=', 1)
+                if len(sps) == 2 and sps[1] == value:
+                    update = 1
+                elif update == 0:
+                    newValue += f'{key}={value}\n'
+                    update = 2
+        else:
+            newValue += line
+
+    if update != 1:
+        if update == 0:
+            newValue += f'\n{key}={value}\n'
+        with open(filename, 'w', encoding='utf-8') as fw:
+            fw.write(newValue)
 
 def removeFakeHwclock():
-    command = 'sudo systemctl status fake-hwclock.service';
-    r = os.popen(command);
-    info = r.readlines();
-    for line in info:
-        if line and line.strip().startswith('Loaded:'):
-            strps = line.split('fake-hwclock.service; ',2);
-            if len(strps) == 2 and strps[1].strip().startswith('enabled'):
-                command = 'sudo systemctl disable fake-hwclock.service';
-                runCommand(command);
+    command = 'sudo systemctl status fake-hwclock.service'
+    with os.popen(command) as r:
+        for line in r:
+            if line.strip().startswith('Loaded:'):
+                if 'enabled' in line:
+                    runCommand('sudo systemctl disable fake-hwclock.service')
 
-def updateHwclockSet() :
-    filename = '/lib/udev/hwclock-set';
-    fr = open(filename,'r');
-    key = '-e /run/systemd/system';
-    update = False;
-    doUpdate = False;
-    try:
-        lines = fr.readlines();
-        newValue = '';
-        for line in lines :
-            if line and line.strip().startswith('if') and line.count(key):
-                update = True;
-                doUpdate = True;
-            if update :
-                newValue += '#';
-            if line and line.strip() == 'fi':
-                update = False;
-            newValue += line;
-        if doUpdate :
-            fw = open(filename,'w');
-            try:
-                fw.write(newValue);
-            finally:
-                fw.close();
-    finally:
-        fr.close();
+def updateHwclockSet():
+    filename = '/lib/udev/hwclock-set'
+    key = '-e /run/systemd/system'
+    update = False
+    doUpdate = False
+    newValue = ''
+
+    with open(filename, 'r', encoding='utf-8') as fr:
+        lines = fr.readlines()
+
+    for line in lines:
+        if line.strip().startswith('if') and key in line:
+            update = True
+            doUpdate = True
+        if update:
+            newValue += '#'
+        if line.strip() == 'fi':
+            update = False
+        newValue += line
+
+    if doUpdate:
+        with open(filename, 'w', encoding='utf-8') as fw:
+            fw.write(newValue)
 
 if __name__ == "__main__":
-    updateConfig();
-    updategpio6();
-#    updategpio13();
-    updateHwclockSet();
-    removeFakeHwclock();
+    updateConfig()
+    updategpio6()
+    # updategpio13()
+    updateHwclockSet()
+    removeFakeHwclock()
